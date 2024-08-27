a79a78165b735b756c8c9bce7cbc2b5a94dd39d2
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-08-27 18:50:59 +0900

    fix

--- src/main/java/net/pitan76/cubicturret/block/DoubleCubicTurretBlock.java
+++ src/main/java/net/pitan76/cubicturret/block/DoubleCubicTurretBlock.java
@@ -1,8 +1,12 @@
 package net.pitan76.cubicturret.block;
 
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
+import net.pitan76.cubicturret.tile.BlockEntities;
 import net.pitan76.mcpitanlib.api.block.CompatibleBlockSettings;
+import org.jetbrains.annotations.Nullable;
 
 public class DoubleCubicTurretBlock extends AbstractCubicTurretBlock {
     public DoubleCubicTurretBlock(CompatibleBlockSettings settings) {
@@ -12,5 +16,10 @@ public class DoubleCubicTurretBlock extends AbstractCubicTurretBlock {
     @Override
     public VoxelShape getShape(Direction direction) {
         return super.getShape(direction);
+    }
+
+    @Override
+    public @Nullable <T extends BlockEntity> BlockEntityType<T> getBlockEntityType() {
+        return (BlockEntityType<T>) BlockEntities.DOUBLE_CUBIC_TURRET.getOrNull();
     }
 }
